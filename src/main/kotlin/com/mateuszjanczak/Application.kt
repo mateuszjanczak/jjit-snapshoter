0328047d9e1c1ceb710bad32e97cb4069ba00d1a
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2025-01-06 19:52:35 +0100

    Init project

---
+++ src/main/kotlin/com/mateuszjanczak/Application.kt
@@ -0,0 +1,11 @@
+package com.mateuszjanczak
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
